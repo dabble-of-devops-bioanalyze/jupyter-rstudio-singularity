72bd501d9d91d1771d6a5b839058f8894d0edeb7
Jillian Rowe <jillian.e.rowe@gmail.com>
2023-01-25 12:04:03 +0300

    fix

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     description="Launch RStudio with Singularity from the command line.",
     entry_points={
         "console_scripts": [
-            "launch-rstudio=jupyter_rstudio_singularity.cli:cli",
+            "launch-rstudio=jupyter_rstudio_singularity.cli:app",
         ],
     },
     install_requires=requirements,
